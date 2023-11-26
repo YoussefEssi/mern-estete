025a523e466613d3e41341a4f480583da6e96ac0
YoussefEssi <yousarsifkorabika@gmail.com*>
2023-11-26 20:22:50 +0100

    complete sign up page funcionality

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -1,6 +1,5 @@
 import User from '../models/user.model.js';
 import bcryptjs from 'bcryptjs';
-import { errorHandler } from '../utils/error.js';
 
 export const signup = async (req, res, next) => {
      const { username, email, password } = req.body;
@@ -10,6 +9,6 @@ export const signup = async (req, res, next) => {
         await newUser.save()
         res.status(201).json('User created successfully!');
      }catch (error) {
-        next(errorHandler(550, 'error from the function'));
+        next(error);
      }
 };
